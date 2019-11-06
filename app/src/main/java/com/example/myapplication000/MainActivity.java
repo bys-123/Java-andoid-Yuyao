22fa879b8d321ec83934d8f40b9bd212aa7f9a1b
Qian Jiao (Beyondsoft Corporation) <v-qijiao@microsoft.com>
2019-11-06 16:06:07 +0800

    Qian20191106

--- app/src/main/java/com/example/myapplication000/MainActivity.java
+++ app/src/main/java/com/example/myapplication000/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.push.Push;
 
 import android.os.Bundle;
 import android.view.View;
@@ -21,10 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //添加按钮
         Button btn_click = (Button)findViewById(R.id.button_001);
+        Button btn2_click = (Button)findViewById(R.id.button_002);
+
         //添加点击事件
         btn_click.setOnClickListener(new MyOnClickListenwe());
+        btn2_click.setOnClickListener(new MyOnClickListenwe2());
 
-        AppCenter.start(getApplication(), "686737c0-e7a9-4f8c-b65b-04d9bf681177", Analytics.class, Crashes.class);
+        AppCenter.start(getApplication(), "a0fdc060-08d7-4920-aa90-f34913041780", Analytics.class, Crashes.class, Push.class);
     }
     private class MyOnClickListenwe implements View.OnClickListener{
         public void onClick(View v){
@@ -32,8 +36,17 @@ public class MainActivity extends AppCompatActivity {
                 Map<String, String> properties = new HashMap<>();
                 properties.put("Category", "Music");
                 properties.put("FileName", "favorite.avi");
-                Analytics.trackEvent("Video clicked");
-                Analytics.trackEvent("Video clicked", properties);
+                Analytics.trackEvent("Event Without properties clicked");
+                Analytics.trackEvent("Event properties clicked", properties);
+            }
+        }
+    }
+
+    private class MyOnClickListenwe2 implements View.OnClickListener{
+        public void onClick(View v){
+            if(v.getId() == R.id.button_002){
+                int x = 1/0;
+                Crashes.generateTestCrash();
             }
         }
     }
